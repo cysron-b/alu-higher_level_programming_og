aa0c4546e40e2be577984302cefd52db2d820301
cysron-b <b.cysron@alustudent.com>
2023-11-23 07:05:33 -0800

    Task 0 -5

--- python-everything_is_object/19-copy_list.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-#!/usr/bin/python3
-def copy_list(l):
-    return l[:]
-=======
-#!/usr/bin/python3
-def copy_list(l):
-    return l[:]
->>>>>>> b5d238a8949409f544dd5d9bd53f297d46c3b0f3
