255b18de4ce49a02d34436ed4c20249c5381d82c
cysron-b <139706271+cysron-b@users.noreply.github.com>
2023-11-29 16:42:21 +0200

    my github

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""
-Script that takes your GitHub credentials (username and password)
-and uses the GitHub API to display your id.
-
-Usage: ./10-my_github.py <GitHub username> <GitHub password>
-  - Uses Basic Authentication to access the ID.
-"""
-from sys import argv
-import requests
-from requests.auth import HTTPBasicAuth
+"""A script that takes GitHub credentials."""
 
 
-if __name__ == "__main__":
-    auth = HTTPBasicAuth(argv[1], argv[2])
-    req = requests.get("https://api.github.com/user", auth=auth)
+if __name__ == '__main__':
+    import requests
+    from requests.auth import HTTPBasicAuth
+    import sys
 
-    print(req.json().get("id"))
+    url = 'https://api.github.com/user'
+    user = sys.argv[1]
+    xyz = sys.argv[2]
+    authori = HTTPBasicAuth(username=user, password=xyz)
+    response = requests.get(url, auth=authori)
+    result = response.json()
+    print(result.get('id'))
