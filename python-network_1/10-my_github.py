392093df869a6b477d6ff05ad1d8df7e1803d83f
cysron-b <139706271+cysron-b@users.noreply.github.com>
2023-11-29 16:53:22 +0200

    Update 10-my_github.py

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
-"""__summary__
-- Write a Python script that takes your GitHub
-- credentials (username and password) and
-- uses the GitHub API to display your id
+"""This module or script
+takes your GitHub credentials (username and personal access token)
+and uses the GitHub API to display your id.
 """
-import requests
+
 import sys
+import requests
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    url = "https://api.github.com/user"
-    auth = (username, password)
+    if len(sys.argv) != 3:
+        print("Usage: {} <username> <access_token>".format(sys.argv[0]))
+        sys.exit(1)
 
-    reqs = requests.get(url, auth=auth)
-    print(reqs.json().get("id"))
+    username = sys.argv[1]
+    access_token = sys.argv[2]
+    url = 'https://api.github.com/user'
+
+    try:
+        response = requests.get(url, auth=(username, access_token))
+        if response.status_code == 200:
+            data = response.json()
+            print(data['id'])
+        else:
+            print(None)
+    except ValueError:
+        print(None)
