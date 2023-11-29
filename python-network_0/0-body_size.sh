201dec46830488983854521fdf6a33079e202480
cysron-b <139706271+cysron-b@users.noreply.github.com>
2023-11-29 17:29:29 +0200

    Update 0-body_size.sh

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash
 # takes URL and displays the size of the response body
 curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
-=======
-#!/bin/bash
-# takes URL and displays the size of the response body
-curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
->>>>>>> 99b8c190bdb9c890b1842a7265f69242eca2195e
