82d4d1db32e10ca7370f9e99475e1b93f40de3d2
cysron-b <b.cysron@alustudent.com>
2023-11-29 17:25:33 +0200

    a

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+# takes URL and displays the size of the response body
 curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
