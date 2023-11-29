38d79beb3c4527829fc63a3efbd6b617fc9b027f
cysron-b <b.cysron@alustudent.com>
2023-11-29 15:43:02 +0000

    a

--- python-network_0/1-body.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# script that send GET request and displays the body of response
-curl -sL $1
--- python-network_0/2-delete.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# script that sends DELETE requests and displays the body of response
-curl -sX DELETE $1
