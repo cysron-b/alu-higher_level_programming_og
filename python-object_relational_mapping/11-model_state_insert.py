59d3e9d5916428c04be9a670def5e404a47bb619
cysron-b <139706271+cysron-b@users.noreply.github.com>
2023-12-13 18:21:29 +0200

    Update 11-model_state_insert.py

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
-"""Add Louisiana to a DB."""
+"""
+Program adds the State object "Lousiana" to the database hbtn_0e_6_usa
+"""
 
 
-import sqlalchemy
+import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sys import argv
-from model_state import Base, State
 
-if __name__ == "__main__":
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                                    argv[2],
-                                                                    argv[3]))
-    Base.metadata.create_all(eng)
-    Session = sessionmaker(bind=eng)
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
     session = Session()
-    new_state = State(name='Louisiana')
-    session.add(new_state)
-    state = session.query(State).filter_by(name='Louisiana').first()
-    print(str(state.id))
+
+    newState = State(name='Louisiana')
+    session.add(newState)
     session.commit()
-    session.close()
+
+    print(newState.id)
